060a3d38277932a1dae6477d392863f0478febb3
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-10 01:01:46 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative '.fancy_dance_spec.rb'
+
+class Kid 
+  
+  extend FancyDance::ClassMethods
+  include FancyDance::Methods
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+  end
+end
